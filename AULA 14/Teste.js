f5044e4a1e0891f5f7674ae37785a8efceae5f32
andrejs233 <84469424+andrejs233@users.noreply.github.com>
2022-05-10 18:56:21 -0300

    Add files via upload

---
+++ AULA 14/Teste.js
@@ -0,0 +1,5 @@
+console.log('Vaicomeçar...')
+for(var c = 1;c <= 5; c++) {//Aqui ele mostra outro laço ded repetição sendo a (variavel; teste log; condição)
+    console.log(c) 
+}
+console.log('FIM')
